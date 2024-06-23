7e285de03fe9b620c3f1629347b361f68582e96c
surajmesta <surajmesta47@gmail.com>
2024-06-23 13:35:44 +0530

    Added Post test with default way

--- src/test/java/com/tests/APITests.java
+++ src/test/java/com/tests/APITests.java
@@ -1,6 +1,8 @@
 package com.tests;
 
 
+import com.utils.CommonUtils;
+import com.utils.PATHS;
 import com.utils.RequestBuilder;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
@@ -11,10 +13,19 @@ public class APITests {
 
     @Test(priority =0)
     public void getTest(){
-        Response response = RequestBuilder.getRequest();
+        Response response = RequestBuilder.getRequest().get(PATHS.PRODUCTS.getUrl());;
         response.prettyPrint();
         response.then().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("results.json"));
         assertThat(response.statusCode()).isNotZero()
                 .isEqualTo(200);
+    }
+
+    @Test(priority = 1)
+    public void postTest1(){
+        String requestPayload = CommonUtils.getPostPayload1();
+        Response response = RequestBuilder.postRequest().body(requestPayload).post(PATHS.PRODUCTS.getUrl());
+        response.prettyPrint();
+        String title = response.jsonPath().get("title");
+        assertThat(title).isEqualTo("test product one");
     }
 }
